0b8fbafdd520324632a96141e2910b5df629ae25
Jyrone Parker <jyrone.parker@gmail.com>
2018-01-27 23:42:08 -0500

    ads get money

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- instatorrent -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7023023584987784"
+     data-ad-slot="2340456025"
+     data-ad-format="auto"></ins>
+<script>
 <send-component></send-component>
 <receive-component></receive-component>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- instatorrents2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7023023584987784"
+     data-ad-slot="7782330870"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 @endsection
